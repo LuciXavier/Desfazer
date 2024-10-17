31226ebc073831a45f3bc814396e3a745ed169c9
LuciXavier <luzinhadesousa@gmail.com>
2024-10-17 20:51:18 -0300

    Delete Elemento.java

--- Elemento.java
+++
@@ -1,9 +0,0 @@
-public class Elemento {
-    Opcao operacao;
-    Elemento proximo;
-
-    public Elemento(Opcao operacao) {
-        this.operacao = operacao;
-        this.proximo = null;
-    }
-}
